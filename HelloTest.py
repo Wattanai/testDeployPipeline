e2daa828391a23efbb746bdbbc22aa65b09b143d
wattanai <wattwalking@gmail.com>
2013-07-26 15:25:24 +0700

    first commit

---
+++ HelloTest.py
@@ -0,0 +1,20 @@
+#__author__ = 'wattanai'
+from teamcity import is_running_under_teamcity
+from teamcity.unittestpy import TeamcityTestRunner
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+
+#    def test_something_wrong(self):
+#        self.assertEquals(True, False)
+
+
+if __name__ == '__main__':
+    if is_running_under_teamcity():
+        runner = TeamcityTestRunner()
+    else:
+        runner = unittest.TextTestRunner()
+    unittest.main(testRunner=runner)
