baaaaebf72ebd5095bc0cd7782d2f03b6cab6de4
wattanai <wattwalking@gmail.com>
2013-08-01 17:48:37 +0700

    fabfile23

--- fabfile.py
+++ fabfile.py
@@ -22,15 +22,9 @@ def testUATdeploy(pwd, gitpp='asdfjkl;\''):
 
     with cd('/home/BuilkTeamCity/builkUAT'):
         run('source bin/activate')
-            #with cd('/home/BuilkTeamCity/builkUAT/testDeployPipeline'):
         run('pip install -r testDeployPipeline/requirement.txt')
         sudo('nginx -s quit')
         sudo('rm /etc/nginx/nginx.conf')
         sudo('cp testDeployPipeline/nginx.conf /etc/nginx/nginx.conf')
-        sudo('supervisorctl -c testDeployPipeline/supervisord.conf restart tornadoes:*')
-                #sudo('supervisorctl reread')
-                #sudo('supervisorctl update')
-                #sudo('supervisorctl start tornadoes:*')
-                #sudo('supervisorctl stop all')
-                #sudo('supervisord -c supervisord.conf')
+        sudo('supervisorctl -c testDeployPipeline/supervisord.conf update tornadoes:*')
         sudo('nginx')
